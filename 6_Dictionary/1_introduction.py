9d1f2bade3faa03da58ef78428e214d2272a205f
Lochier <mohitguptadp7@gmail.com>
2024-09-05 18:07:45 +0530

    Updated Code : Completed array list practice problems, started Dictionary

---
+++ 6_Dictionary/1_introduction.py
@@ -0,0 +1,23 @@
+# Python Dictionary : 
+# A dictionary is a collection which is unordered, 
+# changeble and indexed.
+
+# Example of Dictionary : 
+# Creating Dictionary Normally : 
+# Student Data : 
+Students = {
+    "name" : "Mohit",
+    "age" : 21,
+    "class" : "F.Y",
+    "roll" : 17
+}
+print(Students)
+
+# We can create an empty dictionary using 
+# 'dict()' constructor or using '{}' :
+my_dict = dict()
+print(my_dict)
+
+# Creating a dictionary using 'dict()' :
+eng_spa = dict(one='uno', two='dos', three='tres')
+print(eng_spa)
